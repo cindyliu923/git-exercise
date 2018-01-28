819ea4f56e1854d833c1d2f8a3a52cb54f50bd5c
yvesen <l.sen.t@hotmail.com>
2018-01-28 17:23:42 +0800

    fixed controller and index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # root 代表的路徑、產生對外開放的網址入口
-  root "user#index"
+  root "users#index"
 end
