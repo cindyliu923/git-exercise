e5b8a124968c16b95aa5428b502da996c1916ac4
yvesen <l.sen.t@hotmail.com>
2018-01-28 17:05:06 +0800

    set routes for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # root 代表的路徑、產生對外開放的網址入口
+  root "user#index"
 end
